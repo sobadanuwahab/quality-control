75a4718bf855e5f590cb155d0d567b23b5d6cf72
Soba Danu Wahab <sdanuw@gmail.com>
2025-07-22 06:07:30 +0700

    update dashboard.blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -236,7 +236,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
-        let carouselInstance = null;
+        var carouselInstance = null;
 
         function pauseCarousel() {
             if (carouselInstance) carouselInstance.pause();
