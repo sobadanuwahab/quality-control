69237cffb2cfd80f0634e8d72b24fb4f03b45a25
Soba Danu Wahab <sdanuw@gmail.com>
2025-07-22 06:13:34 +0700

    menambahkan kolom nama_bioskop

---
+++ database/migrations/2025_07_22_061205_add_nama_bioskop_to_admin_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNamaBioskopToAdminTable extends Migration
+{
+  public function up()
+  {
+    Schema::table('admin', function (Blueprint $table) {
+      $table->string('nama_bioskop')->nullable()->after('username'); // setelah kolom username
+    });
+  }
+
+  public function down()
+  {
+    Schema::table('admin', function (Blueprint $table) {
+      $table->dropColumn('nama_bioskop');
+    });
+  }
+}
