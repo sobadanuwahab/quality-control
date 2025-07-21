8ddd9329e02335dcdb50a70845a0760a6a928ec9
Soba Danu Wahab <sdanuw@gmail.com>
2025-07-22 05:26:38 +0700

    nonaktifkan admin_id_to_onesheets

--- database/migrations/2025_06_21_170733_add_admin_id_to_onesheets_table.php
+++ database/migrations/2025_06_21_170733_add_admin_id_to_onesheets_table.php
@@ -4,7 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration {
+  /*
     public function up(): void
     {
         Schema::table('onesheets', function (Blueprint $table) {
@@ -22,4 +24,5 @@ return new class extends Migration {
             $table->dropColumn('admin_id');
         });
     }
+        */
 };
