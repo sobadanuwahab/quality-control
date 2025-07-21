ab02ee1ab15fc323ea50b6b0b2ca5fc8437a081c
Soba Danu Wahab <sdanuw@gmail.com>
2025-07-22 05:29:35 +0700

    nonaktifkan add_role_to_admin

--- database/migrations/2025_06_22_184434_add_role_to_admin_table.php
+++ database/migrations/2025_06_22_184434_add_role_to_admin_table.php
@@ -6,23 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+  /**
+   * Run the migrations.
+   */
+  /*
     public function up(): void
     {
         Schema::table('admin', function (Blueprint $table) {
             $table->string('role')->default('user');
         });
     }
+    */
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('admin', function (Blueprint $table) {
-            $table->dropColumn('role');
-        });
-    }
+  /**
+   * Reverse the migrations.
+   */
+  /*
+  public function down(): void
+  {
+    Schema::table('admin', function (Blueprint $table) {
+      $table->dropColumn('role');
+    });
+  }
+    */
 };
