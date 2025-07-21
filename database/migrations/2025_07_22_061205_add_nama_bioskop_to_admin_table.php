672f4cbabcaea4d566fcaaedd2c069fd874b1e0f
Soba Danu Wahab <sdanuw@gmail.com>
2025-07-22 06:16:44 +0700

    update nama_bioskop

--- database/migrations/2025_07_22_061205_add_nama_bioskop_to_admin_table.php
+++ database/migrations/2025_07_22_061205_add_nama_bioskop_to_admin_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNamaBioskopToAdminTable extends Migration
+return new class extends Migration
 {
   public function up()
   {
@@ -19,4 +19,4 @@ class AddNamaBioskopToAdminTable extends Migration
       $table->dropColumn('nama_bioskop');
     });
   }
-}
+};
