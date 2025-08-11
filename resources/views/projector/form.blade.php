dea5d3f08110cac3df8bc4a9648afaa0d5f16c70
Soba Danu Wahab <sdanuw@gmail.com>
2025-08-11 18:14:34 +0700

    update 11-08-2025

--- resources/views/projector/form.blade.php
+++ resources/views/projector/form.blade.php
@@ -115,4 +115,19 @@
             });
         });
     </script>
+
+    <style>
+        .select2-container--default .select2-selection--single {
+            height: 38px !important;
+            /* Bootstrap .form-control default height */
+            padding: 6px 12px;
+            border: 1px solid #ced4da;
+            border-radius: 0.375rem;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__rendered {
+            line-height: 24px;
+            /* teks di tengah */
+        }
+    </style>
 @endpush
